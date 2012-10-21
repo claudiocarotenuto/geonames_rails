05491a1f9eabec395b975a504d71c9a39b5d2abc
Adrien Di Pasquale <adrien.dipasquale@gmail.com>
2012-10-21 23:35:01 +0900

    Added attributes accessor to the models
    It is now recommended by Rails to use the whitelist to protect
    attributes.

--- lib/generators/geonames_rails/models_templates/models/city.rb
+++ lib/generators/geonames_rails/models_templates/models/city.rb
@@ -1,4 +1,7 @@
 class City < ActiveRecord::Base
   belongs_to :country
   #belongs_to :division
+  attr_accessible :name, :ascii_name, :alternate_name, :latitude, :longitude,
+    :country_iso_code_two_letters, :population, :geonames_timezone_id,
+    :geonames_id, :admin_1_code, :admin_2_code, :admin_3_code, :admin_4_code
 end
--- lib/generators/geonames_rails/models_templates/models/country.rb
+++ lib/generators/geonames_rails/models_templates/models/country.rb
@@ -1,4 +1,7 @@
 class Country < ActiveRecord::Base
   has_many :cities
   #has_many :divisions
-end
+  attr_accessible :iso_code_two_letter, :iso_code_three_letter, :iso_number,
+    :name, :capital, :population, :continent, :currency_name, :currency_code,
+    :geonames_id
+  end
